d371fbf9a24984d45af20f184fe56d7e9f9ad823
DAW101 <DAW101@C2011.iesataulfo.local>
2019-11-21 16:19:17 +0100

    mejora de clase 1/∞

--- AgenciaConciertos/src/agenciaconciertos/CIndividual.java
+++ AgenciaConciertos/src/agenciaconciertos/CIndividual.java
@@ -5,6 +5,7 @@
  */
 package agenciaconciertos;
 import java.sql.Date;
+import java.util.ArrayList;
 /**
  *
  * @author david
@@ -26,5 +27,27 @@ public class CIndividual extends Concierto {
     public String data() {
         return super.data(); //To change body of generated methods, choose Tools | Templates.
     }
-    
+    public CIndividual getCIndividualbyid (long id) {
+        for (int i=0;i<listaobjetos;i++){
+            CIndividual aux=listaobjetos.get(i);
+            if (aux.getID()==id){
+                return aux;
+            } 
+        }
+        return null;
+    }
+    public ArrayList<CIndividual> getAllCindividual(){
+        ArrayList<CIndividual> Lci = new ArrayList<CIndividual>();
+        for (int i=0;i<listaobjetos.size();i++){
+            CIndividual aux=listaobjetos.get(i);
+            Lci.add(aux);
+        }
+        return Lci;
+    }
+    public CIndividual nuevoCIndividual() {
+            System.out.println("Che wacho, ¿Qué día tenés el consierto?");
+            Date fe= Toolbox.realDate(in, "DD-MM-YYYY-HH:MM:SS");
+            f.set.Fecha(fe);
+            return null;
+    }
 }
