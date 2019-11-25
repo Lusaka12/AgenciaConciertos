204301d50e0d9977049b55b808eefe75c24c816e
DAW101 <DAW101@C2011.iesataulfo.local>
2019-11-25 18:05:41 +0100

    metodos creados en clases "momento" y "reportero". :

--- AgenciaConciertos/src/agenciaconciertos/Momento.java
+++ AgenciaConciertos/src/agenciaconciertos/Momento.java
@@ -13,17 +13,60 @@ import java.sql.Time;
  * @author DAW102
  */
 public class Momento {
-    Date hora;
-    String descripcion;
-    public Momento(Date hora, String descripcion) {
+    private Date hora;
+    private String descripcion;
+    private Reportero reportero;
+    
+    public Momento(Date hora, String descripcion, Reportero reportero) {
         this.hora = hora;
         this.descripcion = descripcion;
+        this.reportero= reportero;
     }
     public Momento(Momento m) {
-        this.hora = hora;
-        this.descripcion = descripcion;
+        this.hora = m.getHora();
+        this.descripcion = m.getDescripcion();
+        this.reportero= m.getReportero();
     } 
     public Momento(){
     }
+
+    public Date getHora() {
+        return hora;
+    }
+
+    public void setHora(Date hora) {
+        this.hora = hora;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Reportero getReportero() {
+        return reportero;
+    }
+
+    public void setReportero(Reportero reportero) {
+        this.reportero = reportero;
+    }
     
+    /*public Momento getMomentobyid (long id) {
+        for (int i=0;i<listaobjetos;i++){
+            Momento aux=listaobjetos.get(i);
+            if (aux.getID()==id){
+                return aux;
+    */
+    
+    /*public ArrayList<Momento> getAllMomento(){
+        ArrayList<Momento> LMM = new ArrayList<Momento>();
+        for (int i=0;i<listaobjetos.size();i++){
+            Momento aux=listaobjetos.get(i);
+            LMM.add(aux);
+        }
+        return LMM;
+    }*/
 }
