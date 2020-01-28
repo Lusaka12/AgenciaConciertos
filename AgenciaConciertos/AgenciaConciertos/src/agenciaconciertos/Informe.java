0a44a1779b19b183a25fa1dc87e430839c76eee6
Jairette <56265960+Jairette@users.noreply.github.com>
2020-01-28 18:11:27 +0100

    cambios en main, informe y informeException

--- AgenciaConciertos/AgenciaConciertos/src/agenciaconciertos/Informe.java
+++ AgenciaConciertos/AgenciaConciertos/src/agenciaconciertos/Informe.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -14,8 +14,9 @@ import java.util.Scanner;
  * @version 1.01
  */
 public class Informe {
+
     protected long id;//atributo que sirve para identificarel ID //  valores validos numero entero mayor que 0
-    private boolean revisado;//Campo para indicar si está revisado o no // valores validos true o false. 
+    private boolean revisado = false;//Campo para indicar si está revisado o no // valores validos true o false. 
     private String descripcion;//atributo que sirve para guardar la descripcion // valores validos cadena de caracteres de 20 caracteres
     private Gira gira; //atributo que indica la gira a la que va asociado el informe
 
@@ -35,11 +36,13 @@ public class Informe {
         this.revisado = revisado;
     }
 
-    public String getDescripcion() {
+    public String getDescripcion() throws InformeException {
+        InformeException.validaDescripcion(descripcion);
         return descripcion;
     }
 
-    public void setDescripcion(String descripcion) {
+    public void setDescripcion(String descripcion) throws InformeException {
+        InformeException.validaDescripcion(descripcion);
         this.descripcion = descripcion;
     }
 
@@ -51,72 +54,91 @@ public class Informe {
         this.gira = gira;
     }
 
-    public Informe(long id, String descripcion, Gira gira) {
+    public Informe(long id, String descripcion, Gira gira) throws InformeException {
         this.id = id;
         this.revisado = false;
+        InformeException.validaDescripcion(descripcion);
         this.descripcion = descripcion;
         this.gira = gira;
     }
-    
 
-   public Informe (Informe i) {
+    public Informe(Informe i) throws InformeException {
         this.revisado = i.isRevisado();
+        InformeException.validaDescripcion(descripcion);
         this.descripcion = i.getDescripcion();
         this.gira = i.getGira();
-   }    
-   public Informe(){
-       
-   }
+    }
+
+    public Informe() {
+
+    }
 
     @Override
     public String toString() {
         return "Informe{" + "identificador=" + id + ", revisado=" + revisado + ", descripcion=" + descripcion + ",gira=" + gira + '}';
     }
-   public String data() {
-       
-       return this.getId()+"|"+this.isRevisado()+"|"+this.getDescripcion();
-             
-   }
-   
-   public Informe getInformeById(long id) {
+
+    public String data() throws InformeException {
+        InformeException.validaDescripcion(descripcion);
+        return this.getId() + "|" + this.isRevisado() + "|" + this.getDescripcion();
+
+    }
+
+    public Informe getInformeById(long id) {
         /*for (Informe informe : listaInformes) {
             if (informe.getId() == id) {
                 return informe;
             }
         }*/
-       return null;
-   }
-   
-   public ArrayList<Informe> getAllInforme () {
-      ArrayList<Informe> nuevaListaInforme = new ArrayList<Informe>();
+        return null;
+    }
+
+    public ArrayList<Informe> getAllInforme() {
+        ArrayList<Informe> nuevaListaInforme = new ArrayList<Informe>();
         /*
         for (Informe informe:listaInformes) {
               nuevaListaInformes.add(informe);
           } 
-        */
-        /*Este método recorrerá un ArrayList con todos los informes, comparando 
+         */
+ /*Este método recorrerá un ArrayList con todos los informes, comparando 
         con el id que le introduzcamos, y devolverá el informe si es que existe o 
         nulo si es que no existe*/
         return nuevaListaInforme;
-   }   
-   
-    public Informe nuevoInforme(){
+    }
+
+    public Informe nuevoInforme() throws InformeException {
         Informe informe = new Informe();
-        Scanner in=new Scanner (System.in);
+        Scanner in = new Scanner(System.in);
         boolean confirmacion;
-        do{
-        System.out.println("¿Se ha revisado el informe?");
-        informe.setRevisado(ToolBox.readBoolean());
-        System.out.println("Añade descripción");
-        informe.setDescripcion(in.next());
-        //informe.setGira(Gira.buscaGiraPorNombre(in.next()));
-        confirmacion=ToolBox.readBoolean();
-        }while (confirmacion!=true);
+        do {
+            System.out.println("¿Se ha revisado el informe?");
+            informe.setRevisado(ToolBox.readBoolean());
+            System.out.println("Añade descripción");
+            String descrUser = in.nextLine();
+            InformeException.validaDescripcion(descrUser);
+            informe.setDescripcion(descrUser);
+
+            System.out.println("Introduzca el nombre de la gira");
+            informe.setGira(BaseDatos.buscaGiraByNombre(in.next()));
+            confirmacion = ToolBox.readBoolean();
+        } while (confirmacion != true);
         in.close();
         return informe;
-        }
-       
-       
-   }
-   
+    }
 
+    public boolean revisarInforme() {
+        boolean ret = false;
+        if (this != null) {
+            if (this.isRevisado() == false) {
+                this.setRevisado(true);
+            }
+            System.out.println("El informe " + this.getId() + " ha sido revisado correctamente.");
+            ret=true;
+        } else {
+            System.out.println("No existe el informe.");
+            ret=false;
+        }
+        return ret;
+    }
+
+}
---
+++ AgenciaConciertos/AgenciaConciertos/src/agenciaconciertos/InformeException.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenciaconciertos;
+
+/**
+ *
+ * @author DAW113
+ */
+public class InformeException extends Exception {
+
+    public InformeException(String message) {
+        super(message);
+    }
+    
+    public static void validaDescripcion(String descripcion) throws InformeException{
+        if(descripcion.length()>20){
+            throw new InformeException("La descripcion ocupa mas de 20 caracteres");
+        }
+        for(int i=0;i<descripcion.length();i++){
+           if("0123456789".indexOf(descripcion.charAt(i))!=-1){
+           throw new InformeException("La descripcion contiene algun caracter numerico");
+           }
+       }
+    }
+    
+
+    
+}
